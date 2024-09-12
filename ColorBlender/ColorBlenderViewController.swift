7596c026147563fa9b888bdac8b23fa8a4d712a9
Igor Guryan <sacujo93@yandex.ru>
2024-09-12 23:59:38 +0300

    add UI

--- ColorBlender/ColorBlenderViewController.swift
+++ ColorBlender/ColorBlenderViewController.swift
@@ -6,14 +6,83 @@
 //
 
 import UIKit
+import SnapKit
 
 class ColorBlenderViewController: UIViewController {
+
+    lazy var appNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ColorBlender"
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        return label
+    }()
+    
+    lazy var topStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    lazy var firstColourPickerStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    lazy var secondColourPickerStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    lazy var firstColorPickerButton = UIButton.createColorPickerButton()
+    lazy var secondColorPickerButton = UIButton.createColorPickerButton()
+    lazy var thirdColorPickerButton = UIButton.createColorPickerButton()
+    lazy var fourthColorPickerButton = UIButton.createColorPickerButton()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        setupView()
+        setupLayout()
+        
     }
-
-
+    
+    func setupView() {
+        view.addSubview(appNameLabel)
+        view.addSubview(topStackView)
+        topStackView.addArrangedSubview(firstColorPickerButton)
+        topStackView.addArrangedSubview(secondColorPickerButton)
+    }
+    
+    func setupLayout() {
+        appNameLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+        
+        topStackView.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(30)
+            make.top.equalTo(appNameLabel.snp.bottom).offset(50)
+        }
+        
+        [
+            firstColorPickerButton,
+            secondColorPickerButton,
+            thirdColorPickerButton,
+            fourthColorPickerButton
+        ].forEach({
+            $0.snp.makeConstraints { make in
+                make.size.equalTo(view.frame.width / 3)
+            }
+        })
+        
+        
+    }
+    
 }
-
+#Preview{ColorBlenderViewController()}
---
+++ ColorBlender/UIButton + ext.swift
@@ -0,0 +1,16 @@
+//
+//  UIButton + ext.swift
+//  ColorBlender
+//
+//  Created by Igor Guryan on 12.09.2024.
+//
+
+import UIKit
+
+extension UIButton {
+    static func createColorPickerButton() -> UIButton{
+        let button = UIButton()
+        button.backgroundColor = .red
+        return button
+    }
+}
